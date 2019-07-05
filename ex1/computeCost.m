c55d65f848802cd759b9e7e4701fda9d967cd777
Puiyee Tong <pytong@Puiyees-MacBook-Pro.local>
2019-07-04 23:39:38 -0400

    Solutions for ex1

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -6,17 +6,17 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
-
+error = X * theta - y
+error_squared = (error .^ 2)
+error_squared_sum = sum(error_squared)
+J = error_squared_sum / (2 * m)
 % =========================================================================
 
 end
